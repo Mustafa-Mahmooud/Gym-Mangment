07e85a06f24e0d431d20f1b972037ac0d91b734d
20120 <Mustafa-Mahmooud>
2025-05-12 15:28:12 +0300

    seed

--- Service/obj/Debug/net8.0/Service.AssemblyInfo.cs
+++ Service/obj/Debug/net8.0/Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d30c2b70c6e620d560a397bfff80b00b2d1dffe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b82d4a24509a335b50ee1f4543f8985cade35496")]
 [assembly: System.Reflection.AssemblyProductAttribute("Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
