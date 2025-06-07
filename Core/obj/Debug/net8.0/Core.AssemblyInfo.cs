fa98136af9e367af719fe59d49e9527e14c9a8f3
20120 <Mustafa-Mahmooud>
2025-06-07 22:14:53 +0300

    Payment,ChatAssistant

--- Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b82d4a24509a335b50ee1f4543f8985cade35496")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07e85a06f24e0d431d20f1b972037ac0d91b734d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Presentation/Controllers/PaymentController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Stripe;
+using Presentation.Interfaces;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PaymentController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public PaymentController(IConfiguration configuration)
+        {
+            
+            _configuration = configuration;
+
+        }
+
+        [HttpPost("create-payment-intent")]
+        public ActionResult CreatePaymentIntent([FromBody] PaymentIntentCreateRequest request)
+        {
+            StripeConfiguration.ApiKey = _configuration["Stripe:SecretKey"];
+            var options = new PaymentIntentCreateOptions
+            {
+                Amount = request.Amount,
+                Currency = request.Currency,
+                PaymentMethodTypes = new List<string> { "card" },
+            };
+            var service = new PaymentIntentService();
+            PaymentIntent intent = service.Create(options);
+
+            return Ok(new { clientSecret = intent.ClientSecret });
+        }
+    }
+
+    public class PaymentIntentCreateRequest
+    {
+        public long Amount { get; set; }
+        public string Currency { get; set; }
+    }
+}
